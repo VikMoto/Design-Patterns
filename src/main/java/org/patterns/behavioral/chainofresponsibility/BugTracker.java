6bff4ed5349e8fb82d13ba9c615e7b2466b5e853
ig.basalt@gmail.com <ig.basalt@gmail.com>
2023-07-11 16:03:20 +0300

    Behavioral -> Chain of Responsibility

---
+++ src/main/java/org/patterns/behavioral/chainofresponsibility/BugTracker.java
@@ -0,0 +1,17 @@
+package org.patterns.behavioral.chainofresponsibility;
+
+public class BugTracker {
+    public static void main(String[] args) {
+        Notifier reportNotifier = new SimpleReportNotifier(Priority.ROUTINE);
+        Notifier emailNotifier = new EmailNotifier(Priority.IMPORTANT);
+        Notifier smsNotifier = new SMSNotifier(Priority.ASAP);
+
+        reportNotifier.setNextNotifier(emailNotifier);
+        emailNotifier.setNextNotifier(smsNotifier);
+
+        reportNotifier.notifyManager("Everything is Ok. ", Priority.ROUTINE);
+        reportNotifier.notifyManager("Something went wrong! ", Priority.IMPORTANT);
+        reportNotifier.notifyManager("Huston, we`ve had a problem here!!!", Priority.ASAP);
+
+    }
+}

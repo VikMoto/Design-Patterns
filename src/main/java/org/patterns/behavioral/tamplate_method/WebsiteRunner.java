ed37131e3492ffe26b5352a95e09f9bc7d1d1b24
ig.basalt@gmail.com <ig.basalt@gmail.com>
2023-07-16 20:21:49 +0300

    Behavioral -> Template method

---
+++ src/main/java/org/patterns/behavioral/tamplate_method/WebsiteRunner.java
@@ -0,0 +1,12 @@
+package org.patterns.behavioral.tamplate_method;
+
+public class WebsiteRunner {
+    public static void main(String[] args) {
+        WebsiteTemplate welcomePage = new WelcomePage();
+        WebsiteTemplate newsPage = new NewsPage();
+
+        welcomePage.showPage();
+        System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
+        newsPage.showPage();
+    }
+}
